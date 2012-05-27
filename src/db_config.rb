6bc60b0c4970f6df3e81f1d4bf52edaf5be09bde
Hendrik Weiler <diddlside@googlemail.com>
2012-05-27 21:05:09 +0200

    db_config.rb file into source folder

---
+++ src/db_config.rb
@@ -0,0 +1,76 @@
+module DB
+  class Config
+  	@configfile = "rgss3edb.conf"
+  	@@config = {}
+
+  	def self.parse file
+  		@variables = {}
+  		content = File.read(@configfile)
+  		current_group = false
+  		content.split("\n").each do |line|
+  			if /\[(.*)\]/i.match(line)
+  				name = line.gsub("[","").gsub("]","")
+  				@variables[name] = {}
+  				current_group = name
+  			else
+  				if line == ''
+  					current_group = false
+  					next
+  				end
+  				if(current_group != false)
+  					data = line.split("=")
+  					@variables[current_group][data[0].strip] = data[1].strip
+  				else
+					data = line.split("=")
+  					@variables[data[0].strip] = data[1].strip
+  				end
+  			end
+  		end
+  		@variables
+  	end
+
+  	def self.read
+  		if !File.exists? @configfile
+  			File.open(@configfile,"w") do |file|
+  				file.write(
+  					"[database]\n" +
+  					"selected_db = Database"
+  				)
+  			end
+  		end
+  		@@config = self.parse @configfile
+  		if !Dir.exists? get("database.selected_db")
+  			Dir.mkdir( get("database.selected_db") )
+  		end
+  	end
+
+  	def self.init
+  		read
+  		@@config
+  	end
+
+  	def self.get selector
+  		selecting = @variables
+  		data = selector.split(".")
+  		depth = data.length
+  		if depth == 1
+  			@variables[data[0]]
+  		end
+  		if depth == 2
+  			@variables[data[0]][data[1]]
+  		end
+  	end
+
+  	def self.set key, value
+  		data = key.split(".")
+  		depth = data.length
+  		if depth == 1
+  			@variables[data[0]] = value
+  		end
+  		if depth == 2
+  			@variables[data[0]][data[1]] = value
+  		end
+  	end
+  end
+  Config.init
+end
